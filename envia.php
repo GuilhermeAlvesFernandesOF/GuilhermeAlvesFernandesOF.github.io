62b1a6649f5513956ac5e292c22219d5d8f0b2a9
GuilhermeAlvesFernandesOF <guilhermealvesfernandes.of@gmail.com>
2024-05-31 11:50:43 -0300

    forms

--- envia.php
+++ envia.php
@@ -1,4 +1,21 @@
 <?php
 
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $celular = addslashes($_POST['celular']);
+    $mensagem = addslashes($_POST['mensagem']);
+
+    $para = "guilhermealvesfernandes1901@gmail.com";
+    $assunto = "Formulário-home";
+
+    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Celular: ".$celular."\n"."Mensagem: ".$mensagem;
+
+    $cabeca = "From: guilhermealvesfernandes.19@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-Mail enviado com sucesso!");
+    }else{
+        echo("Houve um erro ao enviar o email!");
+    }
 
 ?>
